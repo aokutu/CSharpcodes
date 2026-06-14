ec114c495a4e25709c1a356278042d6c2713c840
aokutu <andreaokutu@gmail.com>
2026-06-14 12:54:43 +0300

    MAKE A CONSTANT VARIABLE

--- UserInput.cs
+++ UserInput.cs
@@ -1,7 +1,12 @@
 using System;
 					
 Console.WriteLine("ENTER  FIRST NAME ");
-string Firstname;
+const string Firstname;
 
 Firstname = Console.ReadLine();
 Console.WriteLine(Firstname);
+
+/* make  constant varaible*/
+
+const double  Pi = 3.14; 
+Console.WriteLine(Pi);
