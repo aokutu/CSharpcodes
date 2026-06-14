caed6479aeb203cc32fe2e7f8eccb5bbf5f16256
aokutu <andreaokutu@gmail.com>
2026-06-14 15:12:47 +0300

    C# STRUCTURE  DATA TYPES

---
+++ Cshapstructs.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+Details  client;
+
+client.Name1 = "ANDRW";
+client.Name2 ="ANDY";
+
+Console.WriteLine(client.Name1);
+Console.WriteLine(client.Name2);
+
+					
+struct Details 
+{
+public string Name1;
+public string Name2;
+}
+
+
